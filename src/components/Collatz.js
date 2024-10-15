95540aa41cf15e1ebf4830cec3cd5a8e06892075
ItxChewy <aarongarcia1906@gmail.com>
2024-10-15 18:27:03 +0100

    optimizado

--- src/components/Collatz.js
+++ src/components/Collatz.js
@@ -13,32 +13,18 @@ export default class Collatz extends Component {
         e.preventDefault();
         var numero = parseInt(this.cajaNumero.current.value)
         this.state.completo.push(numero);
-        if(numero == 1){
-            this.setState({
-                calculos:numero,
-                completo:this.state.completo
-            })
-        }else {
-            while(numero != 1){
-                if(numero %2 == 0){
-                    var operacion  = numero /2;
-                    numero = operacion; 
-                    this.state.completo.push(numero);
-                    this.setState({
-                        calculos:numero,
-                        completo:this.state.completo
-                    })
-                }else{
-                    var operacion  = numero * 3 + 1;
-                    numero = operacion; 
-                    this.state.completo.push(numero);
-                    this.setState({
-                        calculos:numero,
-                        completo:this.state.completo
-                    })
-                }
+        while (numero !== 1) {
+            if (numero % 2 === 0) {
+                numero = numero / 2;
+            } else {
+                numero = numero * 3 + 1;
             }
-        } 
+            this.state.completo.push(numero); 
+        }
+        this.setState({
+            calculos: numero,
+            completo: this.state.completo
+        }); 
     }
 
     render() {
@@ -51,21 +37,14 @@ export default class Collatz extends Component {
             <button>Calcular...</button>
             <div>
                 <ul>
-                    
                 {
-                    parseInt(this.state.calculos) == 1 && 
-                    (
-                        this.state.completo.map((valor,index) =>{
-                            return(
-                                    <li key={index}>{valor}</li>
-        
+                    this.state.completo.map((valor,index) =>{
+                        return(
+                            <li key={index}>{valor}</li>
                             )
-                        })
-                        
-                    )
+                        })  
                 }
                 </ul>
-                
             </div>
         </form>
       </div>
