c230f0a415e2c417858c91c3bf22c8baf927f479
ItxChewy <aarongarcia1906@gmail.com>
2024-10-15 12:41:51 +0200

    rutas

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+const { Component } = require("react");
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Home Component</h1>
+                <img src=""
+                 style={{width:"150px", height:"150px"}}>
+                 </img>
+            </div>
+        )
+    }
+}
+export default Home
---
+++ src/components/Musica.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Musica extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Musica Component</h1>
+        <img src="" 
+        style={{width:"150px", height:"150px"}}>
+        </img>
+      </div>
+    )
+  }
+}
